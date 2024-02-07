8bc83bf9e312ad0f5d8536e560984d5eba69895f
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-02-07 16:22:40 +0100

    feat: add tasks route

--- application/routes.go
+++ application/routes.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/takahiromitsui/go-task-manager/controller"
+	"github.com/takahiromitsui/go-task-manager/repository/task"
 )
 
 func(app *App) loadRoutes() {
@@ -13,5 +15,16 @@ func(app *App) loadRoutes() {
 	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 	})
+	app.loadTaskRoutes(router)
 	app.router = router
+}
+
+func (app *App) loadTaskRoutes(router chi.Router) {
+	taskController := &controller.Task{
+		Repository: &task.PostgresRepository{
+			DB: app.db,
+		},
+	}
+	router.Post("/tasks", taskController.Create)
+	
 }
